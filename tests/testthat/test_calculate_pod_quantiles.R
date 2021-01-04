a8f6553fb764b214566ac0eb76007233959ba138
k-t-to <kimberlytto@gmail.com>
2021-01-04 17:28:14 -0500

    remove zero doses; remove +1 from log transform

--- tests/testthat/test_calculate_pod_quantiles.R
+++ tests/testthat/test_calculate_pod_quantiles.R
@@ -1,13 +1,13 @@
 context("calculate_pod_quantiles.R")
 
 test_that("Are the predicted PODs as expected?", {
+  set.seed(471833)
   temp_1 <- calculate_pod_quantiles(test_data_tnt, resample_size = 5)
   expect_length(temp_1, 2)
   expect_length(temp_1[[1]], 5)
   expect_length(temp_1[[2]], 3)
-  expect_true(round(temp_1[[2]][2], 4) == 0.4415)
+  expect_true(round(temp_1[[2]][2], 4) == 0.4388)
   
-  set.seed(471833)
   temp_2 <- calculate_pod_quantiles(test_data_toxpod, resample_size = 20)
   expect_length(temp_2[[2]], 3)
   expect_length(temp_2[[1]], 20)
